41f52805d4f88173d801390cd2912c7344277263
wuyou91 <ycx1991@live.com>
2018-03-12 16:34:17 +0800

    右侧内容去粗略布局

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
-window.onload=function(){
-	//设置左边导航区和右边内容区的高度为可视区高度。
-	var oNavLeft=document.getElementById("nav-left");
-	var oContent=document.getElementById("content");
-	oNavLeft.style.height=document.documentElement.clientHeight+"px";
-	oContent.style.height=document.documentElement.clientHeight+"px";
+$(window).ready(function(){
+	//左边导航区的高度为可视区高度。
+	$("#nav-left").height($(window).height());
+	//改变可视区大小，高度自适应
+	$(window).resize(function(){
+		$("#nav-left").height($(window).height());
+	});
 
-	//小屏下点击出现和影藏菜单栏
+	//小屏下点击出现和隐藏菜单栏
 	$(".menu").click(function(){
 		$(".nav-mobile").slideToggle();
-	})
-	//屏幕大小改变影藏菜单栏
+	});
+	//屏幕大小改变隐藏菜单栏
 	$(window).resize(function(){
 		$(".nav-mobile").hide();
-	})
-}
+	});
+})
